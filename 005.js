fde4486b335091ffc33de6210bbc06ac3f82650d
Richard Bidin <richardbidin@outlook.com>
2020-04-08 23:17:37 -0400

    Improved algorithm

--- 005.js
+++ 005.js
@@ -1,21 +1,23 @@
 // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
 
-let number = 1;
+let number = 20;
 let flags = 0;
 let within = 20;
+let found = false;
 
-while(flags < within) {
+while(!found) {
     for(let i = 1; i <= within; i++) {
         if(number % i === 0) {
-            if(flags < within) {
-                flags++;
+            flags++;
+            if(flags === within) {
+                found = true;
             }
         } else {
-            number++;
+            number += 20;
             flags = 0;
         }
     }
 }
 
-console.log(number, flags);
+console.log(number);
